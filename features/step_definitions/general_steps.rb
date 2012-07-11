275eff6f857e6d5b72d8493b7fa423463f313460
Arjan van der Gaag <arjan@Kabisa.nl>
2012-07-11 17:02:54 +0200

    Add feature for packaging

--- features/step_definitions/general_steps.rb
+++ features/step_definitions/general_steps.rb
@@ -11,7 +11,37 @@ Given /^the default "(.*?)" file$/ do |filename|
   write_file filename, read_support_file(filename)
 end
 
+Given /^the default "(.*?)" file with "(.*?)" set to "(.*?)"$/ do |filename, key, value|
+  config = YAML.load(read_support_file(filename))
+  config[key] = value
+  write_file filename, YAML.dump(config)
+end
+
 Then /^the default file "(.*?)" should exist$/ do |filename|
   step %Q{a file named "#{filename}" should exist}
   step %Q{the file "#{filename}" should contain exactly:}, read_support_file(filename)
+end
+
+Then /^the archive "(.*?)" should contain file "(.*?)"$/ do |filename, entry|
+  in_current_dir do
+    Zip::ZipFile.open(filename, Zip::ZipFile::CREATE) do |zipfile|
+      zipfile.get_entry(entry)
+    end
+  end
+end
+
+Given /^a basic project$/ do
+  steps %Q{
+    Given a file named "README.md" with:
+        """
+        README file
+        """
+    And the default "layout.html" file
+    And the default "styles.css" file
+    And the default "config.yml" file
+    And a file named "chapter1.md" with:
+        """
+        Hello, world
+        """
+  }
 end
