fc5635656fe79267161ad59c985b2a6fe82f8395
Arjan van der Gaag <arjan@Kabisa.nl>
2013-03-22 13:54:41 +0100

    Update dependencies to latest Gem versions

--- features/step_definitions/general_steps.rb
+++ features/step_definitions/general_steps.rb
@@ -1,8 +1,3 @@
-# This step is not yet in the released version of Aruba, only in unreleased master branch
-Then /^the file "([^"]*)" should contain:$/ do |file, partial_content|
-  check_file_content(file, partial_content, true)
-end
-
 def read_support_file(filename)
   File.read(File.join(SUPPORT_PATH, filename))
 end
